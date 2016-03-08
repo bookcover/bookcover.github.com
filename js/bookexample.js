5e3b5dc5505e21c1bb1671bd3f1edc58fd292ef5
bookcover <skynrica+githubbookcover@gmail.com>
2016-03-09 05:00:28 +0900

    no message

--- js/bookexample.js
+++ js/bookexample.js
@@ -51,7 +51,7 @@ var books = new Array(
             "title": "역시 내 청춘 러브코메디는 잘못됐다.",
             "labelColor": "",
             "originalTitle": "はり俺の青春ラブコメはまちがっている。",
-            "author": "	와타리 와타루",
+            "author": "와타리 와타루",
             "oAuthor": "渡 航",
             "translator": "박정원",
         },
